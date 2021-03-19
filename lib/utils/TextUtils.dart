00af3e97d3f8f91e1e52c8c6e2e758e38444b52d
Aditya <vns.aditya@ballebaaz.com>
2021-03-19 20:34:21 +0530

    API's now working

--- lib/utils/TextUtils.dart
+++ lib/utils/TextUtils.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:x3/utils/textConstants.dart';
 
 class PText extends StatelessWidget {
   final String textKey;
+  final TextStyle theme;
 
-  const PText({Key key, @required this.textKey}) : super(key: key);
+  const PText({Key key, @required this.textKey, this.theme = const TextStyle()})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<BoxEvent>(
       stream: Hive.box('settings').watch(key: "language"),
       builder: (BuildContext context, AsyncSnapshot<BoxEvent> snapshot) {
-        return Text(TextConstants.getInstance().get(textKey));
+        return Text(
+          TextConstants.getInstance().get(textKey),
+          style: theme,
+        );
       },
     );
   }
