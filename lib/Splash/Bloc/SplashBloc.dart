fdf02189305f978c057a82c6fe731051b7439635
Aditya <vns.aditya@ballebaaz.com>
2021-03-19 20:40:54 +0530

    API's now working

--- lib/Splash/Bloc/SplashBloc.dart
+++ lib/Splash/Bloc/SplashBloc.dart
@@ -1,3 +1,4 @@
+import 'package:hive/hive.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:x3/Splash/model/EnumForStateManagement.dart';
 
@@ -11,5 +12,18 @@ class SplashBloc {
   static SplashBloc getInstance() {
     if (_instance == null) _instance = new SplashBloc();
     return _instance;
+  }
+
+  SplashBloc() {
+    state.add(LoginStates.loading);
+    bool isServerConfigWorking = Hive.box("configuration").get("isConfigured");
+    if (isServerConfigWorking != null && isServerConfigWorking) {
+      bool isLoggedIn = Hive.box("login").get("isLoggedIn");
+      if (isLoggedIn != null && isLoggedIn)
+        state.add(LoginStates.home);
+      else
+        state.add(LoginStates.login);
+    } else
+      state.add(LoginStates.configurationSettings);
   }
 }
